d8e7aba03101f1cc7e2ba5470ca7e21a43be7e82
VincentDS98 <disanto.vincenzo98@gmail.com>
2023-12-14 02:43:25 +0100

    MILESTONE 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-console.log('JS Carousel Monoarray');
+const images = [
+    "./img/01.webp",
+    "./img/02.webp",
+    "./img/03.webp",
+    "./img/04.webp",
+    "./img/05.webp"
+
+];
+
+const itemsContainer = document.querySelector('.items')
+
+for(let i = 0;i<images.length;i++){
+
+    if(i==0) {
+        itemsContainer.innerHTML += `<div class="item active">
+        <img src="${images[i]}" alt="">
+    </div>`;
+
+    }
+    
+    else {
+    itemsContainer.innerHTML += `<div class="item">
+    <img src="${images[i]}" alt="">
+</div>`;
+    }
+}
